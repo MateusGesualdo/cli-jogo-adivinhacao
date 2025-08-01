bfd812eb37001257a4883cc147d5d3f302a32023
cristianetb <cristianetb.infinity@gmail.com>
2025-08-01 23:39:09 +0000

    cor 1

--- jogar.js
+++ jogar.js
@@ -1,4 +1,5 @@
 import { number } from "@inquirer/prompts"
+import chalk from "chalk"
 export async function jogar() {
     try {
         // chamada de api
@@ -9,15 +10,15 @@ export async function jogar() {
 
         while (numeroTentativas > 0) {
 
-            let palpite = await number({ message: "Digite um número de 0 a 100" })
+            let palpite = await number({ message: chalk.yellow("Digite um número de 0 a 100") })  
 
             if (palpite === numeroAleatorio) {
-                console.log("Parabéns, voce acertou!")
+                console.log(chalk.green("Parabéns, voce acertou!"))
                 break
             } else if (palpite > numeroAleatorio) {
-                console.log("Muito alto, tente um número menor")
+                console.log(chalk.blue("Muito alto,") + chalk.rgba(170, 32, 197, 1)("tente um número") + chalk.rgba(0, 185, 231, 1) ("menor"))
             } else if (palpite < numeroAleatorio) {
-                console.log("Muito baixo, tente um número maior")
+                console.logchalk.blue("Muito alto,") + chalk.rgba(170, 32, 197, 1)("tente um número") + chalk.rgba(0, 185, 231, 1) ("menor"))
             } else {
                 numeroTentativas++
                 console.log("Entrada não é um número válido")
