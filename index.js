e4c5c9ea9ccccd3a36fb6d371c012a98c50f9370
MateusGesualdo <57274184+MateusGesualdo@users.noreply.github.com>
2025-08-01 21:10:04 +0000

    closes #2

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ a cada tentativa o programa diz se o numero esta abaixo ou acima do escolhido
 const numeroAleatorio = Math.floor(Math.random() * 101)
 let numeroTentativas = 7
 
-while (numeroTentativas > 0) {
+while (numeroTentativas > 0 ) {
 
     let palpite = await number({message:"Digite um número de 0 a 100"})
 
     if (palpite === numeroAleatorio) {
         console.log("Parabéns, voce acertou!")
+        break
     } else if (palpite > numeroAleatorio) {
         console.log("Muito alto, tente um número menor")
     } else if (palpite < numeroAleatorio) {
