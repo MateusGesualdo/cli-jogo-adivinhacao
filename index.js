4e158f3eaaded17eec53ec5f5999802a2e0eef25
MateusGesualdo <57274184+MateusGesualdo@users.noreply.github.com>
2025-08-01 22:20:45 +0000

    Fixes #3

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import {number} from "@inquirer/prompts"
+import { number } from "@inquirer/prompts"
 
 console.log("Boas vindas ao jogo de adivinhação!")
 
@@ -10,29 +10,37 @@ a cada tentativa o programa diz se o numero esta abaixo ou acima do escolhido
 
 */
 
-const numeroAleatorio = Math.floor(Math.random() * 101)
-let numeroTentativas = 7
 
-while (numeroTentativas > 0 ) {
+try {
+    // chamada de api
+    // solicitar inputs
+    //
+    const numeroAleatorio = Math.floor(Math.random() * 101)
+    let numeroTentativas = 7
 
-    let palpite = await number({message:"Digite um número de 0 a 100"})
+    while (numeroTentativas > 0) {
 
-    if (palpite === numeroAleatorio) {
-        console.log("Parabéns, voce acertou!")
-        break
-    } else if (palpite > numeroAleatorio) {
-        console.log("Muito alto, tente um número menor")
-    } else if (palpite < numeroAleatorio) {
-        console.log("Muito baixo, tente um número maior")
-    } else {
-        numeroTentativas++
-        console.log("Entrada não é um número válido")
+        let palpite = await number({ message: "Digite um número de 0 a 100" })
+
+        if (palpite === numeroAleatorio) {
+            console.log("Parabéns, voce acertou!")
+            break
+        } else if (palpite > numeroAleatorio) {
+            console.log("Muito alto, tente um número menor")
+        } else if (palpite < numeroAleatorio) {
+            console.log("Muito baixo, tente um número maior")
+        } else {
+            numeroTentativas++
+            console.log("Entrada não é um número válido")
+        }
+
+        numeroTentativas = numeroTentativas - 1
     }
 
-    numeroTentativas = numeroTentativas - 1
-}
+    if (numeroTentativas === 0) {
+        console.log(`Tentativas acabaram, o número era ${numeroAleatorio}`)
+    }
 
-if (numeroTentativas === 0) {
-    console.log(`Tentativas acabaram, o número era ${numeroAleatorio}`)
+} catch  {
+console.log('Programa encerrado pelo usuário (vosse )')
 }
-
